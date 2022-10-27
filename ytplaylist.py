41316e6624ba7ffbdf668a3e9c1ca5c7d331111c
0AK80 <111841451+0AK80@users.noreply.github.com>
2022-10-26 20:52:13 -0500

    Update ytplaylist.py

--- ytplaylist.py
+++ ytplaylist.py
@@ -2,19 +2,16 @@ from pytube import Playlist
 
 # AK8
 
-playlist_url = 'https://www.youtube.com/playlist?list='
+playlist_url = input()
 playlist = Playlist(playlist_url)
 
 
 print(len(playlist.video_urls))
 
-# Muestra la  url de los primeros 4 videos de la playlist
-for video in playlist.video_urls[:4]:
-    print(video)
-
 
 for video in playlist.videos:
+    
     video.streams.get_audio_only().download()
 
 
-print('Download Finished')
+print('--------All downloads have finished--------')
